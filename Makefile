49570c281898f3275eb15ac6fa47847031381b80
Anthony Fernandez <anthony@pictarine.com>
2020-05-19 15:15:13 +0200

    Sign and Notarize

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 build:
+	# sudo spctl --master-disable
+	rm -rfv dist
 	npm run release
 
 dev:
@@ -6,6 +8,15 @@ dev:
 
 install:
 	npm install
+
+check-sign:
+	codesign -dv dist/package/PictaSearch-darwin-x64/PictaSearch.app
+
+check-cert:
+	security find-identity -p codesigning -v
+
+check-notarize-logs:
+	xcrun altool --notarization-info $(build_id) -u $(username) -p $(psswd)
 
 clear:
 	rm -rf ~/Library/Saved\ Application\ State/com.electron.dext.savedState/
--- scripts/package.js
+++ scripts/package.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const packager = require('electron-packager');
 const chalk = require('chalk');
+const signAsync = require('electron-osx-sign').signAsync
+const { notarize } = require('electron-notarize');
 const pkg = require('../package.json')
 
 const DEFAULT_OPTIONS = {
@@ -10,6 +12,7 @@ const DEFAULT_OPTIONS = {
   icon: path.resolve(__dirname, '..', 'resources', 'icon.icns'),
   overwrite: true,
   prune: true,
+  appBundleId: 'com.pictarine.pictasearch',
   ignore: [
     '__mocks__/',
     'docs/.*.md',
@@ -25,6 +28,23 @@ const DEFAULT_OPTIONS = {
     'webpack.config.prod.js',
     'webpack.config.dev.js',
   ],
+  osxSign: {
+    'provisioning-profile': path.resolve(__dirname, '..', 'embedded.provisionprofile'),
+    identity: process.env.identity,
+    entitlements: path.resolve(__dirname, '..', 'entitlements.mac.plist'),
+    'entitlements-inherit': path.resolve(__dirname, '..', 'entitlements.inherit.mac.plist'),
+    type: 'development',
+    platform: 'darwin',
+    'gatekeeper-assess': true,
+    'hardened-runtime': true,
+    app: path.resolve(__dirname, '..', 'dist', 'package', `${pkg.name}-darwin-x64`, `${pkg.name}.app`)
+  },
+  osxNotarize: {
+    appBundleId: 'com.pictarine.pictasearch',
+    appPath: path.resolve(__dirname, '..', 'dist', 'package', `${pkg.name}-darwin-x64`, `${pkg.name}.app`),
+    appleId: process.env.appleId,
+    appleIdPassword: process.env.appleIdPassword,
+  }
 };
 
 /**
